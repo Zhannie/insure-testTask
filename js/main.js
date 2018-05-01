a2d825aec91823e88b557932e56f380d9f9e3bfe
Zhanna <zkobzaryova@gmail.com>
2018-05-01 15:38:14 +0300

    tablet plus mobile v

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
-var navElements = document.getElementsByClassName('nav-link'),
+var navElements  = document.getElementsByClassName('nav-link'),
 
-	tableBtn    = document.getElementById('table'),
-	cardBtn     = document.getElementById('card');
+	tableBtn     = document.getElementById('table'),
+	cardBtn      = document.getElementById('card'),
+	sectionItems = document.getElementsByTagName('section'),
+
+	mmBtn 		 = document.getElementById('mm-btn'),
+	mMenu 		 = document.querySelector('ul.mobile-menu');
 
 for(let i = 0; i < navElements.length; i++) {
 	navElements[i].onclick = function() {
@@ -11,12 +15,19 @@ for(let i = 0; i < navElements.length; i++) {
 		this.classList.add('active');
 	};
 };
-
 tableBtn.onclick = function() {
-
+	sectionItems[0].classList.add('tableView');
 };
 
 cardBtn.onclick = function() {
+	sectionItems[0].classList.remove('tableView');
+};
 
+mmBtn.onclick = function() {
+	if(mMenu.classList.contains('opened')) {
+		mMenu.classList.remove('opened');
+	}else{
+		mMenu.classList.add('opened');
+	}
 };
 
